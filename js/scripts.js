90ebede1efb4486d7fe865899db7107a31258597
Vanessa Su <vnessa.su@gmail.com>
2021-04-02 12:42:29 -0400

    Modify the numberToProcessedArray function to pass the input of 4 and input of 23 test cases

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 // Business Logic
 function numberToProcessedArray(number){
-  let processedArray = [0];
+  let processedArray = [];
+  for(let currentNumber = 0; currentNumber <= number; currentNumber++){
+    let numberString = currentNumber.toString();
+    if(numberString.includes("3")){
+      processedArray.push("Won't you be my neighbor?");
+    } else if(numberString.includes("2")){
+      processedArray.push("Boop!");
+    } else if(numberString.includes("1")){
+      processedArray.push("Beep!");
+    } else {
+      processedArray.push(currentNumber);
+    }
+  }
   console.log(processedArray);
   return processedArray;
 }
