b97360801ee8bf3a31744ced0d48b8e6d746dafb
Vanessa Su <vnessa.su@gmail.com>
2021-04-02 15:18:50 -0400

    Modify jQuery function to display reversed array if reverse order checkbox is selected

--- js/scripts.js
+++ js/scripts.js
@@ -49,6 +49,11 @@ $(document).ready(function(){
     console.log(nameInput);
     console.log(reversedOrderSelected);
     const outputStringArray = numberToProcessedArray(numberInput, nameInput);
-    $("#resultsDisplay").text(outputStringArray);
+    if(reversedOrderSelected){
+      let reversedArray = reverseArrayOrder(outputStringArray);
+      $("#resultsDisplay").text(reversedArray);
+    } else {
+      $("#resultsDisplay").text(outputStringArray);
+    }
   });
 });
